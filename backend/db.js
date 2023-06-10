80e5030852079d0dd52cf219e17011fb8d8a76ae
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-10 22:20:35 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appName=MongoDB%20Compass&directConnection=true&ssl=false";
 
-const connectToMongo = () =>{
-    mongoose.connect(mongoURI,).then(()=>console.log('succesfully connected to mongo')).catch((err)=>{console.error(err);});
-}
+
 
 module.exports = connectToMongo;
